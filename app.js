370fb160cf24499cf172e145599bfedc0f9723f1
Tolgahan Tolu <tolgahanntolu@gmail.com>
2023-09-02 18:00:06 +0300

    views directory has been prepared with ejs

--- app.js
+++ app.js
@@ -9,5 +9,47 @@ connect("mongodb://localhost/misfit-db", {
   useUnifiedTopology: true,
 }).then(() => console.log("DB Connected Succesfully 🥳"));
 
+//! Template Engine
+app.set("view engine", "ejs");
+
+//! Middlewares
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    page_name: "home",
+  });
+});
+app.get("/about", (req, res) => {
+  res.render("about", {
+    page_name: "about",
+  });
+});
+app.get("/service", (req, res) => {
+  res.render("service", {
+    page_name: "service",
+  });
+});
+app.get("/news", (req, res) => {
+  res.render("news", {
+    page_name: "news",
+  });
+});
+app.get("/trainer", (req, res) => {
+  res.render("trainer", {
+    page_name: "trainer",
+  });
+});
+app.get("/gallery", (req, res) => {
+  res.render("gallery", {
+    page_name: "gallery",
+  });
+});
+app.get("/contact", (req, res) => {
+  res.render("contact", {
+    page_name: "contact",
+  });
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server started on post ${port} 🚀`));
